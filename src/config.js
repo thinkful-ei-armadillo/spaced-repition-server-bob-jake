bce7f6aa8d8f097aa7732a6b2eabc71c085b6399
Juicebox Jones <jderhalli@gmail.com>
2019-04-15 14:23:51 -0700

    adjustment for local testing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://viva-mexico.now.sh',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL
