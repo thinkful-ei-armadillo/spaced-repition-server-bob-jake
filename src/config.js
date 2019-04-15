9d62c1a52857069fb419c4ba9823b63e230447ab
Juicebox Jones <jderhalli@gmail.com>
2019-04-15 12:35:05 -0700

    updated with client origin

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://viva-mexico.now.sh',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL
